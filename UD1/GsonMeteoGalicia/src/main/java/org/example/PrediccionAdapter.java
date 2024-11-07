99a1ad44823c23b47ab973602272057b70b90a9d
A22DavidCR <A22DavidCR@iessanclemente.net>
2024-11-07 11:08:18 +0100

    Prediction Advanced

--- UD1/GsonMeteoGalicia/src/main/java/org/example/PrediccionAdapter.java
+++ UD1/GsonMeteoGalicia/src/main/java/org/example/PrediccionAdapter.java
@@ -1,53 +1,29 @@
 package org.example;
 
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
+import com.google.gson.*;
 
 import java.lang.reflect.Type;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 public class PrediccionAdapter implements JsonDeserializer<Prediccion> {
-
     @Override
-    public Prediccion deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-        JsonObject mainJsonObject = jsonElement.getAsJsonObject();
+    public Prediccion deserialize(JsonElement e, Type t, JsonDeserializationContext c) throws JsonParseException {
+        JsonObject ojson = e.getAsJsonObject();
+        JsonObject jsonPredicion = ojson.getAsJsonObject("predConcello");
 
-        // Extrae el objeto Concello
-        JsonObject concelloJson = mainJsonObject.getAsJsonObject("concello");
-        Concello concello = new Concello();
-        if (concelloJson != null) {
-            concello.setNomeConcello(concelloJson.get("nome").getAsString());
-            concello.setIdConcello(concelloJson.get("id").getAsInt());
+        // Obtener nombre y id del concello
+        String nomeConcello = (jsonPredicion.has("nome")
+                && !jsonPredicion.get("nome").isJsonNull()) ? jsonPredicion.get("nome").getAsString() : null;
+        int idConcello = jsonPredicion.get("idConcello").getAsInt();
+
+        // Crear la instancia de Prediccion
+        Prediccion prediccion = new Prediccion(new Concello(nomeConcello, idConcello));
+
+        // Obtener y agregar la lista de predicciones diarias
+        JsonArray array = jsonPredicion.getAsJsonArray("ListaPreDiaConcello");
+        for (JsonElement elemento : array) {
+            prediccion.addPredDiaConcello(c.deserialize(elemento.getAsJsonObject(), PrediccionDia.class));
         }
-
-        // Extrae la lista de predicciones diarias
-        List<PrediccionDia> listaPredDiaConcello = new ArrayList<>();
-        for (JsonElement diaElement : mainJsonObject.getAsJsonArray("prediccionDias")) {
-            PrediccionDia dia = new PrediccionDia();
-            JsonObject diaJson = diaElement.getAsJsonObject();
-
-            // Maneja los posibles valores nulos
-            if (diaJson.has("dataPredicion") && !diaJson.get("dataPredicion").isJsonNull()) {
-                dia.setDataPredicion(LocalDate.parse(diaJson.get("dataPredicion").getAsString()));
-            }
-            if (diaJson.has("temperaturaMaxima") && !diaJson.get("temperaturaMaxima").isJsonNull()) {
-                dia.setTemperaturaMaxima((int) diaJson.get("temperaturaMaxima").getAsDouble());
-            }
-            if (diaJson.has("temperaturaMinima") && !diaJson.get("temperaturaMinima").isJsonNull()) {
-                dia.setTemperaturaMinima((int) diaJson.get("temperaturaMinima").getAsDouble());
-            }
-
-            listaPredDiaConcello.add(dia);
-        }
-
-        Prediccion prediccion = new Prediccion();
-        prediccion.setConcello(concello);
-        prediccion.setListaPredDiaConcello(listaPredDiaConcello);
 
         return prediccion;
     }
