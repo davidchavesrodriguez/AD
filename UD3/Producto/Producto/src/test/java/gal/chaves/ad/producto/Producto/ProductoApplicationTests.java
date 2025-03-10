2bf5eda11c2dc6f90418b1285955b1b1264f73c5
A22DavidCR <A22DavidCR@iessanclemente.net>
2025-03-10 09:53:37 +0100

    ?

---
+++ UD3/Producto/Producto/src/test/java/gal/chaves/ad/producto/Producto/ProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package gal.chaves.ad.producto.Producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
